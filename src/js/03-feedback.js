b8e2d315bced64f7bfe25ff8c21c21ed8554a28c
Mykola28112002 <95100219+Mykola28112002@users.noreply.github.com>
2022-06-20 13:25:28 +0300

    import

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
 
+import throttle from 'lodash.throttle';
+
+const feedBackFormRef = document.querySelector('.feedback-form');
+const emailRef = feedBackFormRef.elements.email;
+const messageRef = feedBackFormRef.elements.message;
+const formData = {};
+const KEYMSG = '"message';
+
+feedBackFormRef.addEventListener('input', throttle(onFormInput, 500));
+
+feedBackFormRef.addEventListener('submit', onFormSubmit);
+
+getLocaleStorageData();
+
+setformData();
+
+function getLocaleStorageData() {
+  if (localStorage.getItem(KEYMSG)) {
+    const storageData = JSON.parse(localStorage.getItem(KEYMSG));
+    emailRef.value = storageData.email;
+    messageRef.value = storageData.message;
+  }
+}
+
+function setformData() {
+  formData.email = emailRef.value;
+  formData.message = messageRef.value;
+}
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(KEYMSG, JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  if (formData.email !== '' && formData.message !== '') {
+    console.log(formData);
+
+    e.currentTarget.reset();
+    localStorage.removeItem(KEYMSG);
+    setformData();
+  }
+}
